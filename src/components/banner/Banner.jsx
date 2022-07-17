1b70787d4ef78a06ec681b94414c48b49d2b7c7c
Mutahar aslam <mutahar.aslam@retailo.co>
2022-07-18 00:32:20 +0500

    fix

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -13,7 +13,7 @@ const Banner = () => {
   return (
     <>
       <div className="w-full">
-        <div className="banner mt-8 relative h-128 bg-heroImg bg-no-repeat mx-auto flex flex-col items-start pt-8 sm:pt-16 md:pt-20 pl-8 sm:pl-16 md:pl-24 lg:pl-32 xl:pl-48 w-full" >
+        <div className="banner relative h-128 bg-heroImg bg-no-repeat mx-auto flex flex-col items-start pt-8 sm:pt-16 md:pt-20 pl-8 sm:pl-16 md:pl-24 lg:pl-32 xl:pl-48 w-full" >
           <img className="mr-2 lg:mr-12 mt-2 w-7/12 sm:w-6/12 md:w-4/12 lg:w-3/12 lg:mt-12 absolute right-0 bottom-0" src={RogerAvatar} alt="bg" />
          
           <div className="w-11/12 lg:w-2/6 sm:w-3/6 text-white sm:text-left ">
